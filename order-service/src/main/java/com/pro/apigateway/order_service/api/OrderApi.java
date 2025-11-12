5fb9f403d0cb67d2bf8751029ada42db835594cf
Default Name <letsdevapps>
2025-11-12 20:20:52 -0300

    Alteração do prefixo de rotas do api gateway

--- order-service/src/main/java/com/pro/apigateway/order_service/api/OrderApi.java
+++ order-service/src/main/java/com/pro/apigateway/order_service/api/OrderApi.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class OrderApi {
 
-    @GetMapping("/orders")
-    public String getOrders() {
-        return "Order Service - List of Orders";
-    }
+//    @GetMapping("/")
+//    public String getOrders() {
+//        return "Order Service - List of Orders";
+//    }
 }
